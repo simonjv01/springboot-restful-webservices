bedf45953d02c341d6e937b98b9eceabaea1cbc8
Simon Vargas <sjvargas01@gmail.com>
2023-12-21 15:18:45 -0500

    updated to add modelMapper

--- src/main/java/com/example/springbootrestfulwebservices/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/springbootrestfulwebservices/service/impl/UserServiceImpl.java
@@ -6,6 +6,7 @@ import com.example.springbootrestfulwebservices.mapper.UserMapper;
 import com.example.springbootrestfulwebservices.repository.UserRepository;
 import com.example.springbootrestfulwebservices.service.UserService;
 import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,16 +19,20 @@ public class UserServiceImpl implements UserService {
 
     private UserRepository userRepository;
 
+    private ModelMapper modelMapper;
+
     @Override
     public UserDto createUser(UserDto userDto) {
 
         // Convert UserDto into User JPA Entity
-        User user1 = UserMapper.mapToUser(userDto);
+       // User user1 = UserMapper.mapToUser(userDto);
+        User user1 = modelMapper.map(userDto, User.class);
 
         User savedUser = userRepository.save(user1);
 
         // Convert user1 JPA entity to UserDto
-        UserDto savedUserDto = UserMapper.maptToUserDto(savedUser);
+        // UserDto savedUserDto = UserMapper.mapToUserDto(savedUser);
+        UserDto savedUserDto = modelMapper.map(savedUser, UserDto.class);
         return savedUserDto;
     }
 
@@ -35,7 +40,7 @@ public class UserServiceImpl implements UserService {
     public UserDto getUserByID(Long userId) {
         Optional<User> optionalUser = userRepository.findById(userId);
         User user = optionalUser.get();
-        return UserMapper.maptToUserDto(user);
+        return modelMapper.map(user, UserDto.class);
     }
 
     @Override

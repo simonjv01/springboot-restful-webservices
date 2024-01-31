7fc9297d43cb2f70de7b6c287096c48b012b7b1f
Simon Vargas <sjvargas01@gmail.com>
2024-01-31 18:53:44 -0500

    created exception file

---
+++ src/main/java/com/example/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,17 @@
+package com.example.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    private String resourceName;
+    private String fieldName;
+    private Long fieldValue;
+
+
+    public ResourceNotFoundException(String resourceName, String fieldName, Long fieldValue) {
+        super(String.format("%s not found with %s : '%s'", resourceName, fieldName, fieldValue));
+        
+
+    }
+
+    
+}
